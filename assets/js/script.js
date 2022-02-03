c9ff17fd92cec04531d60bf3f09c376d7f98e6e1
tecarts <axibal.lloyd@gmail.com>
2022-02-02 21:00:33 -0800

    added to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,35 @@
 // TODO: Declare any global variables we need
-
+let headsRolls = 0
+let tailsRolls = 0
 
 document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
     // You can remove it once you see it in your browser console in the developer tools
     console.log('Hi')
 
+    // Determine result using Math.random() which returns a value between 0 and 0.5
+    // Therefore, it has a 50% chance to be below 0.5
+        document.getElementById('flip').addEventListener('click', () => {
+            let flippedHeads = Math.random() < 0.5
+        })
+
+    // Take different actions for heads vs tails flipped
+    if(flippedHeads){
+        // Display image and message as heads
+        document.getElementById('penny-image').src = 'assets/image/penny-heads.jpg'
+        document.getElementById('message').textContent = 'You Flipped Heads!'
+    
+        // Add one at the count of number of heads flipped
+        headsRolls +=1
+    } 
+    else {
+        // Display image and message as tails
+        document.getElementById('penny-image').src = 'assets/image/penny-tails.jpg'
+        document.getElementById('message').textContent = 'You Flipped Tails!'
+
+        // Add one to the count of number of heads flipped
+        tailsRolls += 1
+    }
     // TODO: Add event listener and handler for flip and clear buttons
 
     // Flip Button Click Handler
